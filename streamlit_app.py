f1359d998e72aaeb41e00d82dd0e523905c6781e
AasimAcc <162302189+AasimAcc@users.noreply.github.com>
2024-04-17 10:37:33 +0530

    Update prompt basic

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,14 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the PeopleTools docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
         # index = VectorStoreIndex.from_documents(docs)
-       service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo",temperature=0.5,system_prompt=" Task: Answer technical questions about Oracle PeopleSoft PeopleTools. Role: You are a large language model trained on a massive dataset of technical information related to Oracle PeopleSoft PeopleTools. Input: User-provided questions related to Oracle PeopleSoft PeopleTools functionalities, configurations, or best practices. Output: 1. Provide factual and informative answers based on your knowledge and avoid generating responses that are not grounded in reality. 2.For questions related to Oracle PeopleSoft PeopleTools functionalities, configurations, or best practices: Provide factual and informative answers based on your knowledge and avoid generating responses that are not grounded in reality. 3.For greetings or small talk: Respond politely with a greeting (e.g., Hello! How can I help you today?). 4.For all other questions not related to Oracle PeopleSoft PeopleTools: Apologize politely and explain your area of expertise. 5. If the users question is unclear or ambiguous, you may rephrase the question for clarification before attempting to answer. If the users question requires specific details about their PeopleTools environment or configuration, you may indicate that additional information is needed to provide a more accurate response." ))
-       index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-       return index
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Oracle PeopleSoft People tools and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
 
 index = load_data()
 
